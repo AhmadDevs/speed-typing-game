1dbc8cecf61a02c79508d42f5110c5bb8e5d3bcb
Ahmad Ul Haq <sheikhahmad54@outlook.com>
2022-08-07 02:16:05 +0500

    ädded state hook to keep track of textarea input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.scss';
+import { useState } from 'react';
 
 function App() {
+  const [text, setText] = useState("");
+    
+  const handleChange = (event) => {
+      const {name, value} = event.target;
+      setText(value);
+  }
   return (
     <div class="app-container">
         <h1>How fast do you type?</h1>
-        <textarea value="" />
+        <textarea
+            type="text"
+            value={text}
+            name="text"
+            onChange={handleChange}
+        />
         <h4>Amount of time remaining: </h4>
         <button>Start</button>
         <h1>Word Count: </h1>
