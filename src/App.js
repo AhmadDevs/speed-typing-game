a894d029d4faab205030e239306ed8136e379b1c
Ahmad Ul Haq <sheikhahmad54@outlook.com>
2022-08-07 03:44:32 +0500

    added timer functionality on button click

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.scss';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
   const [text, setText] = useState("");
-    
+  const [timeRemaning, setTimeRemaning] = useState(15);
+  const [isRunning, setIsRunning] = useState(false);
+
+  const handleStart = () => {
+    console.log("working");
+    setIsRunning(true);
+  }
   const handleChange = (event) => {
     const {name, value} = event.target;
     setText(value);
@@ -14,8 +20,22 @@ function App() {
     const words = text.trim().split(" ");
     return words.filter(word => word !== "");
   }
+
+  useEffect(() => {
+    if (timeRemaning > 0 && isRunning){
+      setTimeout( () => {
+        setTimeRemaning(time => time -1);
+      }, 1000) 
+    }else if(isRunning === 0){
+      setIsRunning(false);
+    }
+  }, 
+  [timeRemaning, isRunning])
+
+  console.log("hook == ", timeRemaning);
+
   return (
-    <div class="app-container">
+    <div className="app-container">
         <h1>How fast do you type?</h1>
         <textarea
             type="text"
@@ -23,8 +43,8 @@ function App() {
             name="text"
             onChange={handleChange}
         />
-        <h4>Amount of time remaining: </h4>
-        <button>Start</button>
+        <h4>Amount of time remaining: {timeRemaning} </h4>
+        <button onClick={handleStart}>Start</button>
         <h1>Word Count: </h1>
     </div>   
   );
